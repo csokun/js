cdaad96e90838b065525f4dedf9c76feb19aab11
Chorn Sokun <chornsokun@gmail.com>
2015-04-23 14:58:47 +1000

    Update table.js

--- table.js
+++ table.js
@@ -1,30 +1,34 @@
 (function ( $ ) {
  
-    $.fn.collect = function() {
-		var tmp_csv = $('#tmp_csv');
-		if (!tmp_csv) {
-			$("body").append($("<div id='tmp_csv' />"));
-			tmp_csv = $('#tmp_csv');
-		}
+    $.fn.collect = function($prefix) {
+		document._tmp_csv = [];
 		
 		// extracting data off the table
 		var rows = $('tr.gridrow').length;
-		var cols =  $('tr:first td').length;
-		
-		
+		var cols =  $('tr.gridrow:first td').length;
+		var line = "";
 		for (var r = 0; r < rows; r ++) {
-			var line = "";
+			line = ($prefix !== undefined) ? $prefix + ",": "";
 			for( var c = 0; c < cols; c++) {
-				line = $('#list_holder > div.grid_holder > table > tbody > tr:nth-child(" + (r + 1) + ") > td:nth-child(" + (c +1) + ")").text() + ",";
+				var el = '#list_holder > div.grid_holder > table > tbody > tr:nth-child(' + (r + 1) + ') > td:nth-child(' + (c +1) + ')';
+				line += $(el).text() + ',';
 			}
-			tmp_csv.attr('value', tmp_csv.attr('value')  + "\r\n"  + line);
+			// stripout the last comma
+			if (line.length > 0) {
+				line = line.substr(0, line.length - 1);
+			}
+			
+			// push data to array collection
+			document._tmp_csv.push( line );
 		}
 		
         return this;
     };
  
-	$.fn.getCSV = function() {
-		return $('#tmp_csv').attr('value');
+	$.fn.printCsvString = function() {
+		for (var i = 0; i < document._tmp_csv.length; i++ ) {
+			console.log( document._tmp_csv[i] );
+		}
 	}
  
 }( jQuery ));
